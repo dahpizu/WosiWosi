e7b5dc69879295160722b3a5068ba6f5f20203b6
Oladapo Hamzat <hamzathorladapo@yahoo.com>
2025-08-08 10:59:47 +0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/WosiWosi/", // MUST match your repo name (case sensitive)
-  plugins: [react()],
-});
