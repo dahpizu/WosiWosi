5a387b6ec976b62e09b5134858e18241f4ac9d26
Oladapo Hamzat <hamzathorladapo@yahoo.com>
2026-04-24 01:49:49 +0400

    fix: dynamic base path for dev and production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/WosiWosi/", // 👈 THIS is the fix
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/WosiWosi/" : "/",
   plugins: [react()],
-});
+}));
